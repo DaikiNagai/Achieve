d3fa31d3e6abd5eb3e916768a47f3b3ad9031385
EC2 Default User <ec2-user@ip-172-31-29-97.us-east-2.compute.internal>
2018-10-26 06:05:38 +0000

    first commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,30 @@
+class ContactsController < ApplicationController
+ 
+  def new
+    if params[:back] # あー、confirmからは params[:back]が存在するのかな。なら、cが邪魔だね
+      @contacts = Contact.new(contacts_params)
+    else
+      @contacts = Contact.new
+    end
+  end
+
+  def create
+    @contacts = Contact.new(contacts_params)
+    #Contact.create(contacts_params)
+    if @contacts.save
+      redirect_to root_path, success:"お問い合わせが完了しました！"
+    else
+      render'new'
+    end
+  end
+  
+  def confirm
+    @contacts = Contact.new(contacts_params)
+    render :new if @contacts.invalid?
+  end
+  
+  private
+    def contacts_params
+      params.require(:contact).permit(:name, :email, :content)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  #get 'top/index'
+  #get 'contacts/new'
+  #get 'contacts/create'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy] do
+      collection do
+        post :confirm
+      end
+    end
+    #get 'blogs' => 'blogs#index'
+    
+    root 'top#index'
+    
+    resources :contacts, only: [:new, :create] do
+      collection do
+        post :confirm
+      end
+    end
+    
+end
