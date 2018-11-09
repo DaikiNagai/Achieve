a4ea2e8f7d6c917a08517673e61d7db95eb452a9
永井大基 <01prankster@gmail.com>
2018-11-09 19:49:35 +0900

    AddNametoUser

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_09_080418) do
+ActiveRecord::Schema.define(version: 2018_11_09_104430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2018_11_09_080418) do
     t.string "provider", default: "", null: false
     t.string "image_url"
     t.string "avatar"
+    t.string "name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
