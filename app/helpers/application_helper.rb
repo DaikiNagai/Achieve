ef7b8782b223f693405903d94ef50a6606c14e90
永井大基 <01prankster@gmail.com>
2018-11-13 11:32:21 +0900

    index_image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,16 @@
 module ApplicationHelper
   def profile_img(user)
-    return image_tag(user.avatar, alt: user.name)if user.avatar?
-
-    unless user.provider.blank?
-      img_url = user.image_url
+    #return image_tag(user.avatar, alt: user.name) if user.avatar?
+    if user.avatar?
+      image_tag(user.avatar.to_s, alt: user.name)
     else
-      img_url = 'no_image.png'
+
+      unless user.provider.blank?
+        img_url = user.image_url
+      else
+        img_url = 'no_image.png'
+      end
+      image_tag(img_url, alt: user.name)
     end
-    image_tag(img_url, alt: user.name)
   end
 end
